9e84310fa16e7ea7b9961fb82ac4448b4060eb01
anasrazaofficial <anasbukhari4114@gmail.com>
2026-05-11 02:11:36 +0500

    Updagte model

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import numpy as np
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:5173"])
 
 # Load trained model
 model = joblib.load("crypto_fraud_model.pkl")
